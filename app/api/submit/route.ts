7414a2621c9a4fc27480ebf6c786d85172d1e199
Jesse Razo <teamrazo@gmail.com>
2026-09-25 17:45:47 +0000

    fix: valid process.env references and tailwind CSS directives

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
 
     const locationId = process.env.GHL_LOCATION_ID || 'ltcv3MxZCgXvlwLhieDR';
 
-    const apiKey = ***;
+    const apiKey = *** || process.env.GHL_WCOC_API_KEY || '';
 
 
 
